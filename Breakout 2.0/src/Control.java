b5b642dd41d1097383c37f57f74d01f175fb55df
Sebastian <34139737+Koldkiller@users.noreply.github.com>
2018-03-05 17:40:12 +0100

    updated

--- Breakout 2.0/src/Control.java
+++ Breakout 2.0/src/Control.java
@@ -11,24 +11,38 @@ import java.awt.event.KeyListener;
  */
 public class Control implements KeyListener {
 
+	Model model;
+
+	public Control(Model model) {
+		// TODO Auto-generated constructor stub
+
+		this.model = model;
+
+	}
+
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getKeyChar() == 'A' || e.getKeyChar() == 'a') {
+			double xPos = model.paddlePosition.getX();
+			model.paddlePosition.setX(xPos - 4);
+		}
+		if (e.getKeyChar() == 'D' || e.getKeyChar() == 'd') {
+			double xPos = model.paddlePosition.getX();
+			model.paddlePosition.setX(xPos + 4);
+		}
 	}
-
-	
 
 }
