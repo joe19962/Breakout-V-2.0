90466118c278ab06109a9c85304cf55bc3c2d868
joe19962 <36859466+joe19962@users.noreply.github.com>
2018-03-06 19:51:31 +0100

    Update View.java

--- Breakout 2.0/src/View.java
+++ Breakout 2.0/src/View.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
@@ -18,61 +17,70 @@ public class View extends GraphicsProgram {
 	/**
 	 * 
 	 */
-	private GOval ball = new GOval(0,0, Model.BALL_RAD, Model.BALL_RAD);
-	private GRect paddel = new GRect(0,0, Model.PADDLE_WIDTH, Model.PADDLE_HEIGHT);
-	private GRect[] brick = new GRect[100];
+	public GOval ball = new GOval(0, 0, Model.BALL_RAD, Model.BALL_RAD);
+	public GRect paddel = new GRect(0, 0, Model.PADDLE_WIDTH, Model.PADDLE_HEIGHT);
+	public GRect[] brick = new GRect[100];
 	private Model model;
-	private Control control;
 
 	/**
 	 * @param model
 	 * 
 	 */
 
-
 	public void update() {
 		setSize(Model.WINDOW_WIDTH, Model.WINDOW_HEIGHT);
 		addPaddle();
 		addBall();
-		addBricks();
-		
-		
+		removeBricks();
+
+	}
+
+	private void removeBricks() {
+		for (int i = 0; i < brick.length; i++) {
+			if (model.brickPosition[i] != null && model.activeBricks[i] == false) {
+				model.brickPosition[i] = null;
+				remove(brick[i]);
+			}
+		}
 	}
 
 	public View(Model model, Control control) {
 		// TODO Auto-generated constructor stub
 		addKeyListeners(control);
 		this.model = model;
-		this.control = control;
-		
+		addBricks();
+
 	}
+
 	private void addBricks() {
-		for(int i = 0; i < brick.length; i++) {
+		for (int i = 0; i < brick.length; i++) {
 			if (model.activeBricks[i] != false) {
-				GRect brickI = new GRect(model.brickPosition[i].getX(), model.brickPosition[i].getY(), Model.BRICK_WIDTH, Model.BRICK_HEIGHT);
-				brickI.setFilled(true);
-				brickI.setFillColor(new Color(0, 255 , 0));
-				add(brickI);
-			} else if (brick[i] != null){
-				remove(brick[i]);
+				brick[i] = new GRect(model.brickPosition[i].getX(), model.brickPosition[i].getY(), Model.BRICK_WIDTH,
+						Model.BRICK_HEIGHT);
+				brick[i].setFilled(true);
+				brick[i].setFillColor(new Color(0, 255, 0));
+				add(brick[i]);
+
 			}
 		}
 	}
+
 	private void addPaddle() {
-		
+
 		paddel.setFilled(true);
 		paddel.setFillColor(Color.BLACK);
 		add(paddel);
 		paddel.setLocation(model.paddlePosition.getX(), model.paddlePosition.getY());
-		
+
 	}
+
 	private void addBall() {
 		ball.setFilled(true);
 		ball.setFillColor(Color.RED);
 		add(ball);
-		
+
 		ball.setLocation(model.ballPosition.getX(), model.ballPosition.getY());
-		
+
 	}
 
 	public void gamewin() {
