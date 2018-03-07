9f602947041c29929283a77d6d7ac1c80f43d041
joe19962 <36859466+joe19962@users.noreply.github.com>
2018-03-07 17:40:44 +0100

    Update Control.java

--- Breakout 2.0/src/Control.java
+++ Breakout 2.0/src/Control.java
@@ -36,11 +36,15 @@ public class Control implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
+		if (e.getKeyChar() == 'B'|| e.getKeyChar() == 'b')  {
+			model.paddlePosition.setX(model.ballPosition.getX()-(Constants.PADDLE_WIDTH/2));
+		}
+		
 		if (model.paddlePosition.getX() >= 0 && (e.getKeyChar() == 'A' || e.getKeyChar() == 'a')) {
 			double xPos = model.paddlePosition.getX();
 			model.paddlePosition.setX(xPos - 10);
 		}
-		if (model.paddlePosition.getX() + Model.PADDLE_WIDTH <= Model.WINDOW_WIDTH
+		if (model.paddlePosition.getX() + Constants.PADDLE_WIDTH <= Constants.WINDOW_WIDTH
 				&& (e.getKeyChar() == 'D' || e.getKeyChar() == 'd')) {
 			double xPos = model.paddlePosition.getX();
 			model.paddlePosition.setX(xPos + 10);
