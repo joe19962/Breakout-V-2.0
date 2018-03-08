6bdeb880fbcde4c2848184df62f8e1c367b5f4a9
Sebastian <34139737+Koldkiller@users.noreply.github.com>
2018-03-08 14:42:47 +0100

    Final Update View

--- Breakout 2.0/src/View.java
+++ Breakout 2.0/src/View.java
@@ -1,24 +1,16 @@
 import java.awt.Color;
-import acm.graphics.GOval;
-import acm.graphics.GRect;
-import acm.io.IODialog;
+import java.awt.Font;
+import acm.graphics.*;
 import acm.program.*;
-
-/**
- * 
- */
 
 /**
  * @author J.Masche S.Scheible
  *
  */
 public class View extends GraphicsProgram {
-
-	/**
-	 * 
-	 */
-	public GOval ball = new GOval(0, 0, Constants.BALL_RAD, Constants.BALL_RAD);
-	public GRect paddel = new GRect(0, 0, Constants.PADDLE_WIDTH, Constants.PADDLE_HEIGHT);
+	// GObjects for view
+	public GOval ball = new GOval(0, 0, Model.BALL_RAD, Model.BALL_RAD);
+	public GRect paddel = new GRect(0, 0, Model.PADDLE_WIDTH, Model.PADDLE_HEIGHT);
 	public GRect[] brick = new GRect[100];
 	private Model model;
 
@@ -28,11 +20,42 @@ public class View extends GraphicsProgram {
 	 */
 
 	public void update() {
-		setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+		setSize(Model.WINDOW_WIDTH, Model.WINDOW_HEIGHT);
 		addPaddle();
 		addBall();
 		removeBricks();
 
+	}
+
+	private void addBall() {
+		ball.setFilled(true);
+		ball.setFillColor(Color.RED);
+		add(ball);
+
+		ball.setLocation(model.ballPosition.getX(), model.ballPosition.getY());
+
+	}
+
+	private void addPaddle() {
+
+		paddel.setFilled(true);
+		paddel.setFillColor(Color.BLACK);
+		add(paddel);
+		paddel.setLocation(model.paddlePosition.getX(), model.paddlePosition.getY());
+
+	}
+
+	private void addBricks() {
+		for (int i = 0; i < brick.length; i++) {
+			if (model.activeBricks[i] != false) {
+				brick[i] = new GRect(model.brickPosition[i].getX(), model.brickPosition[i].getY(), Model.BRICK_WIDTH,
+						Model.BRICK_HEIGHT);
+				brick[i].setFilled(true);
+				brick[i].setFillColor(new Color(0, 255, 0));
+				add(brick[i]);
+
+			}
+		}
 	}
 
 	private void removeBricks() {
@@ -52,48 +75,20 @@ public class View extends GraphicsProgram {
 
 	}
 
-	private void addBricks() {
-		for (int i = 0; i < brick.length; i++) {
-			if (model.activeBricks[i] != false) {
-				brick[i] = new GRect(model.brickPosition[i].getX(), model.brickPosition[i].getY(), Constants.BRICK_WIDTH,
-						Constants.BRICK_HEIGHT);
-				brick[i].setFilled(true);
-				brick[i].setFillColor(new Color(0, 255, 0));
-				add(brick[i]);
-
-			}
-		}
-	}
-
-	private void addPaddle() {
-
-		paddel.setFilled(true);
-		paddel.setFillColor(Color.BLACK);
-		add(paddel);
-		paddel.setLocation(model.paddlePosition.getX(), model.paddlePosition.getY());
-
-	}
-
-	private void addBall() {
-		ball.setFilled(true);
-		ball.setFillColor(Color.RED);
-		add(ball);
-
-		ball.setLocation(model.ballPosition.getX(), model.ballPosition.getY());
-
-	}
-
 	public void gamewin() {
 		// TODO Auto-generated method stub
-		IODialog winner = new IODialog();
-		ball.setVisible(false);
-		winner.print("You Win!");
+		removeAll();
+		GLabel winner = new GLabel("You are a Winner!!!", Model.WINDOW_WIDTH / 2, Model.WINDOW_HEIGHT / 2);
+		winner.setFont(Font.SANS_SERIF);
+		add(winner);
+
 	}
 
 	public void gameOver() {
-		IODialog gameOver = new IODialog();
-		ball.setVisible(false);
-		gameOver.print("You Lost!");
+		removeAll();
+		GLabel loser = new GLabel(" You Lose!", Model.WINDOW_WIDTH / 2, Model.WINDOW_HEIGHT / 2);
+		loser.setFont(Font.SANS_SERIF);
+		add(loser);
 	}
 
 }
